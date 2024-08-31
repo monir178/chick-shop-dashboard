61f3667e03da8e4d253540c5f1b2ef1628377f37
monir178 <monir.mzs17@gmail.com>
2024-08-31 19:26:22 +0600

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,89 +1,80 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
+  prefix: "",
   theme: {
-    fontSize: {
-      "heading1-bold": [
-        "50px",
-        {
-          lineHeight: "100%",
-          fontWeight: "700",
-        },
-      ],
-      "heading2-bold": [
-        "30px",
-        {
-          lineHeight: "100%",
-          fontWeight: "700",
-        },
-      ],
-      "heading3-bold": [
-        "24px",
-        {
-          lineHeight: "100%",
-          fontWeight: "700",
-        },
-      ],
-      "heading4-bold": [
-        "20px",
-        {
-          lineHeight: "100%",
-          fontWeight: "700",
-        },
-      ],
-      "body-bold": [
-        "18px",
-        {
-          lineHeight: "100%",
-          fontWeight: "700",
-        },
-      ],
-      "body-semibold": [
-        "18px",
-        {
-          lineHeight: "100%",
-          fontWeight: "600",
-        },
-      ],
-      "body-medium": [
-        "18px",
-        {
-          lineHeight: "100%",
-          fontWeight: "500",
-        },
-      ],
-      "base-bold": [
-        "16px",
-        {
-          lineHeight: "100%",
-          fontWeight: "600",
-        },
-      ],
-      "base-medium": [
-        "16px",
-        {
-          lineHeight: "100%",
-          fontWeight: "500",
-        },
-      ],
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
     },
     extend: {
       colors: {
-        "white-1": "#F8F8F8",
-        "grey-1": "#616161",
-        "grey-2": "#E5E7EB",
-        "blue-1": "#005EBE",
-        "blue-2": "#E9F5FE",
-        "blue-3": "#F5F7F9",
-        "red-1": "#FF0000",
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
-};
-export default config;
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default config
